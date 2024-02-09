c8fcbabdd030a5333a737f998fd2257038c4cba4
George Oastler <goastler4@gmail.com>
2024-02-09 09:36:58 +0000

    remove file server

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/client/index.ts',
+  entry: './src/index.ts',
   output: {
     filename: 'bundle.js',
     path: __dirname + '/docs'
   },
   plugins: [
     new CopyPlugin({
-      patterns: [{ from: 'src/client/index.html', to: 'index.html' }]
+      patterns: [{ from: 'src/index.html', to: 'index.html' }]
     })
   ],
   module: {
